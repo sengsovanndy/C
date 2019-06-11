3a7147a0d90675db0f1cd000b4726becdb00dbb0
sengsovanndy <47348537+sengsovanndy@users.noreply.github.com>
2019-06-12 00:42:13 +0700

    Add files via upload

---
+++ 01findtheareaoftherectangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	int a,b,res;
+	printf("Find the area of a rectangle\n");
+	printf("Enter the lenght of the rectangle");
+	scanf("%d",&a);
+	printf("Enter the width of the rectangle");
+	scanf("%d",&b);
+	res=a*b;
+	printf("The result of the area of a rectangle is=%d",res);
+	
+}
