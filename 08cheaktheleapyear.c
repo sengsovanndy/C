3edbe3716754b6cf3b4987b1f73ee7146ef5c8fe
sengsovanndy <47348537+sengsovanndy@users.noreply.github.com>
2019-06-12 00:47:16 +0700

    Add files via upload

---
+++ 08cheaktheleapyear.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+	 int a;
+	 printf("enter the year to check does it leab year or not\n");
+	 printf("enter the year here");
+	 scanf("%d",&a);
+	 if(a%4==0)
+	 {
+	 	printf("The given year is leab year");
+	 }
+	 else
+	 {
+	 	printf("The given number is not the leab year");
+	 }
+	 
+}
