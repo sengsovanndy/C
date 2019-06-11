5cd5e37e36f1b4b3401239a4a10ed1aacaf1d8eb
sengsovanndy <47348537+sengsovanndy@users.noreply.github.com>
2019-06-12 00:48:18 +0700

    Add files via upload

---
+++ 09checknegativeorpositive.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	printf("enter the number to check does it negative or postive\n");
+	printf("enter your number here");
+	scanf("%d",&a);
+	if(a<0)
+	{
+		printf("the given number is negative");
+	}
+	else
+	{
+		printf("the given number is postive");
+	}
+	
+}
